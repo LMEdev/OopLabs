cc2143b2c1710e745b7f97a1121d5b3d6589dc40
n <malinov-ll@mail.ru>
2024-09-18 19:47:15 +0400

    lab_1 task_4

---
+++ src/main/java/ru/leonid/labs/lab_1/task_4/MyFirstProgram.java
@@ -0,0 +1,22 @@
+package ru.leonid.labs.lab_1.task_4;
+
+import ru.leonid.labs.lab_1.task_4.myfirstpackage.*;
+
+
+class MyFirstProgram {
+    public static void main(String[] s) {
+        MyFirstPackage o = new MyFirstPackage(1, 2);
+        System.out.println(o.action());
+
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 8; j++) {
+                o.setA(i);
+                o.setB(j);
+                System.out.print(o.action() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
